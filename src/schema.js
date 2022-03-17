1967d6c6d4920704f631634bc13e47ee43da206d
Jevans13 <Jack13evans@gmail.com>
2022-03-17 12:32:35 +0000

    Add decsriptions in schema.js

--- src/schema.js
+++ src/schema.js
@@ -3,16 +3,21 @@ import { gql } from 'apollo-server-core'
 const typeDefs = gql`
 
     type CoreUnit {
+        "The Core Unit ID code"
         Code: String
+        "The name of the Core Unit"
         Name: String
     }
 
     type Query {
+        "Retrieve information for all Core Units, filterable using the fields below"
         coreUnits: [CoreUnit],
+        "Retrieve information for a single Core Unit, use the argument field to enter Core Unit code"
         coreUnit(Code: String): [CoreUnit]
     }
 
     type Mutation {
+        "Add a Core Unit to the schema"
         addCoreUnit(Code: String, Name: String): String
     }
 `;
