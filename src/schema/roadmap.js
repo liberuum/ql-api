32c579320d63ffcb5ccad639ca51d49ec7ae877c
pcatana <catanapetru@gmail.com>
2022-03-23 11:16:31 +0000

    added rest of model for roadmap model

--- src/schema/roadmap.js
+++ src/schema/roadmap.js
@@ -16,6 +16,84 @@ export const typeDefs = gql`
         DONE
     }
 
+
+    type RoadmapStakeholder {
+        id: ID!
+        roadMapId: Int!
+        stakeholderRoleId: Int!
+    }
+
+    type Stakeholder {
+        id: ID!
+        name: String!
+        stakeholderContributorId: Int!
+        stakeholderCuCode: String!
+    }
+
+    type StakeholderRole {
+        id: ID!
+        stakeholderRoleName: String!
+    }
+
+    type Output {
+        id: ID!
+        outPutName: String!
+        roadMapId: Int!
+        outputUrl: String!
+    }
+
+    type Milestone {
+        id: ID!
+        roadMapId: Int!
+        outputUrl: String!
+    }
+
+    type Task {
+        id: ID!
+        parentId: Int!
+        taskName: String!
+        taskStatus: TaskStatus!
+        ownerStakeholderId: Int!
+        startDate: String!
+        target: String!
+        completedPercentage: Float!
+        confidenceLevel: ConfidenceLevel!
+        comments: String!
+    }
+
+    enum TaskStatus {
+        TO_DO
+        IN_PROGRESS
+        DONE
+        WONT_DO
+        BLOCKED
+    }
+    
+    enum ConfidenceLevel {
+        HIGH
+        MEDIUM
+        LOW
+    }
+
+    type TaskOutput {
+        id: ID!
+        taskId: Int!
+        outputUrl: String!
+    }
+
+    type Review {
+        id: ID!
+        taskId: Int!
+        reviewDate: String!
+        reviewOutcome: ReviewOutcome!
+    }
+
+    enum ReviewOutcome {
+        RED
+        YELLOW
+        GREEN
+    }
+
     type Query {
         roadMaps: [Roadmap]
     }
