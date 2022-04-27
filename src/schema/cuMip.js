d34eb41cb2bd7f9631c69b028babd45e379f8d2e
pcatana <catanapetru@gmail.com>
2022-04-27 17:40:09 +0100

    added url param to cuMip

--- src/schema/cuMip.js
+++ src/schema/cuMip.js
@@ -12,6 +12,7 @@ export const typeDefs = gql`
         rejected: String
         obsolete: String
         mipStatus: CuMipStatus
+        url: String
         mip39: [Mip39]
         mip40: [Mip40]
         mip41: [Mip41]
