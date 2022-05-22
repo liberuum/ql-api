87f12d56b8c3970888b0e9404429312d9eb51600
pcatana <catanapetru@gmail.com>
2022-05-22 14:39:35 +0100

    setup

---
+++ src/schema/auth/data.js
@@ -0,0 +1,10 @@
+export const users = [
+    {
+        id: '1',
+        cuId: '1',
+        email: 'pete@pete.pete',
+        password: 'pete123',
+        roles: ['editor'],
+        permissions: ['write:own_cu']
+    }
+]
