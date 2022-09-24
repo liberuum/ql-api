28fc5175709d9532389a0f5252a29b83c2c81f71
wkampmann <wouter@ksdc.be>
2022-09-24 17:09:57 +0200

    Make the BudgetStatement API module dependent on Auth so it can check write permissions.

--- src/modules/default.config.js
+++ src/modules/default.config.js
@@ -2,7 +2,7 @@ export default {
     Auth: { enabled: true },
     ClientVersion: { enabled: true },
     CoreUnit: { enabled: true },
-    BudgetStatement: { enabled: true, require: ['CoreUnit'] },
+    BudgetStatement: { enabled: true, require: ['Auth', 'CoreUnit'] },
     Mip: { enabled: true, require: ['CoreUnit'] },
     Roadmap: { enabled: true, require: ['CoreUnit'] },
 };
