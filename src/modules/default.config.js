81b47fba8237c917110b4352386f41bbb686dff7
wkampmann <wouter@ksdc.be>
2022-09-24 13:55:47 +0200

    Switch to dynamic loading of the schema modules.

---
+++ src/modules/default.config.js
@@ -0,0 +1,8 @@
+export default {
+    Auth: { enabled: true },
+    BudgetStatement: { enabled: true },
+    ClientVersion: { enabled: true },
+    CoreUnit: { enabled: true },
+    Mip: { enabled: true },
+    Roadmap: { enabled: true },
+};
