4ab076f437b5e2c9867cdb491da612f74f9f433c
Jevans13 <Jack13evans@gmail.com>
2022-11-08 20:16:38 -0500

    Update V0.4 migration file

--- migrations/new/202211130900_release_202211.js
+++ migrations/new/202211130900_release_202211.js
@@ -5,21 +5,31 @@ import knex from "knex";
 export async function up(knex) {
 
     //Add the CoreUnit.legacyBudgetStatementUrl attribute to the Budget Statment table
+    console.log("Adding the CoreUnit.legacyBudgetStatementUrl attribute...");
     await knex.schema.alterTable('CoreUnit', function (table) {
         table.string('legacyBudgetStatementUrl');
     })
 
     //Add System to the Role table
+    console.log("Adding the Role.system attribute...");
     await knex.schema.alterTable('Role', function (table) {
     table.boolean('system');
     })
 
+    //Add active attribute the Core Unit table
+    console.log("Adding the User.active attribute...")
+    await knex.schema.alterTable('User', function (table) {
+    table.boolean('active');
+    })
+
     //Add the Auditor Role to the Auditor Table
+    console.log("Adding the Auditor value to the Role table..")
    knex.insert({
         roleName: 'Auditor'
     }).into('Role');
 
     //Add the CoreUnit/Audit permission to the RolePermission table
+    console.log("Adding the CoreUnit/Audit value to the RolePermission table..")
     knex.insert({
         roleId: 2,
         resource: 'CoreUnit',
@@ -44,6 +54,12 @@ export async function down(knex) {
     knex('RolePermission')
     .where({ permission: 'Audit' })
     .del();
+
+    console.log('Deleting User.active attribute...');
+
+    await knex.schema.alterTable('User', function (table) {
+        table.dropColumn('active');
+      });
     
     console.log('Deleting Role.system attribute...');
 
@@ -54,7 +70,7 @@ export async function down(knex) {
     console.log('Deleting CoreUnit.legacyBudgetStatementUrl attribute...')
 
     await knex.schema.alterTable('CoreUnit', function (table) {
-        table.dropColumn('legacyBudgetSatementUrl');
+        table.dropColumn('legacyBudgetStatementUrl');
       });
     
 };
